af44890c234987143856a3be56495078715f07ab
Jerry Davis <jerry.davis@smail.astate.edu>
2020-12-23 09:36:29 -0600

    Removed Temporary Login/Logout Form Invocation for Testing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useAuthContext } from "../utils/AuthContext";
 import { LOGIN_USER, LOGOUT_USER } from "../utils/actions";
-import LoginForm from "../components/LoginForm";
-import Logout from "../components/Logout";
 
 const Landing = () => {
 
@@ -14,17 +12,16 @@ const Landing = () => {
       {authState.username ? (
         <div>Welcome back, {authState.username}</div>
       ) : (
-        <LoginForm/>
+        <div>You are not logged in.</div>
       )}
       {authState.username ? (
-        /*<button onClick={() => authDispatch({ type: LOGOUT_USER })}>Logout</button>*/
-        <Logout/>
+        <button onClick={() => authDispatch({ type: LOGOUT_USER })}>Logout</button>
       ) : (
         <button
           onClick={() =>
             authDispatch({ type: LOGIN_USER, username: "Test", admin: false })
           }>
-          Old Test Login
+          Login
         </button>
       )}
     </div>
