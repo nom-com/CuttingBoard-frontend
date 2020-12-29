066b3d0a18aaaeed61e153db654847a9c6668b54
Jerry Davis <jerry.davis@smail.astate.edu>
2020-12-29 11:40:13 -0600

    Added Ingredients list to View Recipe Page

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,15 +2,12 @@ import React from "react";
 import { useAuthContext } from "../utils/AuthContext";
 import { LOGOUT_USER } from "../utils/actions";
 import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
 
 const Logout = () => {
     const [authState, authDispatch] = useAuthContext();
     
     return (
-        <Container>
-            <Button onClick={() => authDispatch({ type: LOGOUT_USER })}>Logout</Button>
-        </Container>
+        <Button onClick={() => authDispatch({ type: LOGOUT_USER })}>Logout</Button>
     )
 }
 
--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -9,13 +9,15 @@ const dummyRecipeObj = {
   createdBy: "Nom.com Selects",
   creationDate: "12/25/2020",
   category: "French Cuisine",
-  instructions: ["Give Up", "Order Fast Food", "????", "Profit"]
+  instructions: ["Give Up", "Order Fast Food", "????", "Profit"],
+  ingredients: [{"ingredient": "Butter", "amount": "2 tbsp(30mL)"}, {"ingredient": "Flour", "amount": "2 tbsp(30mL)"}, {"ingredient": "Salt", "amount": "1/2 tsp(2.5mL)"}, {"ingredient": "Milk", "amount": "3/4 cup (175mL)"}, {"ingredient": "Egg Yolk(s)", "amount": "4"}, {"ingredient": "Egg White(s)", "amount": "2", }, {"ingredient": "Cream of Tartar", "amount": "1/4 tsp(1.25mL)"}]
 };
 
 //Displays a chosen Recipe with id matching www.url.com/recipe/{id}
 const Recipe = (props) => {
   //Contains all Relevant Data for Recipe Display
-  const [recipeData, setRecipeData] = useState({id: null, imageLocation: "", title: "", description: "", createdBy: "", creationDate: "", category: "", instructions: []});
+  const [recipeData, setRecipeData] = useState({id: null, imageLocation: "", title: "", description: "", createdBy: "", creationDate: "", category: "", instructions: [], ingredients: []});
+  
   //  useEffect to API get by ID etc...
   useEffect(() => {
     //API Call using matching parameter
@@ -39,12 +41,22 @@ const Recipe = (props) => {
         <br/><img src={recipeData.imageLocation}/>
         <h1>Description:</h1>
         {recipeData.description}
+        <div id="ingredients-list">
+        <h1>Ingredients</h1>
+          <ul>
+          {recipeData.ingredients.map(ingredient => (
+            <li key={ingredient.ingredient}>
+              {ingredient.amount} {ingredient.ingredient}
+            </li>
+          ))}
+          </ul>
+        </div>
+        <div id="instructions-list">
         <h1>Instructions</h1>
-        <div>
           <ol>
-          {recipeData.instructions.map(instruction => (
+            {recipeData.instructions.map(instruction => (
             <li>
-              {instruction}
+              {(instruction)}
             </li>
           ))}
           </ol>
