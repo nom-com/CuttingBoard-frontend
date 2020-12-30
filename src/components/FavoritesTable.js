df8835d974c8ea33ef2331ec124eb109ff0c87d4
dansirdan <daniel.monteton@gmail.com>
2020-12-30 14:56:54 -0800

    Adding fade, spinner loading, and state/dispatch calls

--- src/components/FavoritesTable.js
+++ src/components/FavoritesTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Table from "@material-ui/core/Table";
 import Grid from "@material-ui/core/Grid";
 import TableBody from "@material-ui/core/TableBody";
@@ -10,12 +10,16 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import TrashIcon from "@material-ui/icons/Delete";
-import { Link } from "react-router-dom";
-// import EditIcon from "@material-ui/icons/Edit";
-// import Avatar from "@material-ui/core/Avatar";
 import Tooltip from "@material-ui/core/Tooltip";
+import { Link } from "react-router-dom";
+import { useStoreContext } from "../utils/GlobalState";
+import { SET_FAVORITES, LOADING } from "../utils/actions";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Fade from "@material-ui/core/Fade";
 
 const FavoritesTable = () => {
+  const [state, dispatch] = useStoreContext();
+
   const favoriteRecipes = [
     {
       id: 0,
@@ -40,58 +44,97 @@ const FavoritesTable = () => {
     },
   ];
 
+  //
+  const setFavorites = () => {
+    dispatch({ type: LOADING, loading: true });
+    setTimeout(function () {
+      dispatch({
+        type: SET_FAVORITES,
+        favorites: favoriteRecipes,
+      });
+    }, 1500);
+  };
+
+  // WILL BE AN API CALL
+  const removeFavorite = id => {
+    const shorterFavs = state.favorites.filter(fav => fav.id !== id);
+    dispatch({
+      type: SET_FAVORITES,
+      favorites: shorterFavs,
+    });
+  };
+
+  useEffect(() => {
+    setFavorites();
+  }, []);
+
   return (
     <React.Fragment>
-      {favoriteRecipes.length > 0 ? (
+      {state.loading ? (
         <Grid container item direction='row' justify='center'>
-          <Grid item xs={12}>
-            <TableContainer component={Paper}>
-              <Table aria-label='simple table'>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Image</TableCell>
-                    <TableCell>Title</TableCell>
-                    <TableCell>Category</TableCell>
-                    <TableCell>Public/Private</TableCell>
-                    <TableCell>Actions</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {favoriteRecipes.map(recipe => (
-                    <TableRow key={recipe.id}>
-                      <TableCell component='th' scope='row'>
-                        <Tooltip title='View Recipe'>
-                          <Link to={`/recipe/${recipe.id}`}>
-                            <img src={recipe.img} alt='placeholder' />
-                          </Link>
-                        </Tooltip>
-                      </TableCell>
-                      <TableCell>{recipe.title}</TableCell>
-                      <TableCell>{recipe.category}</TableCell>
-                      <TableCell>
-                        {recipe.public ? "public" : "private"}
-                      </TableCell>
-                      <TableCell>
-                        <Tooltip title='Remove from Favorites'>
-                          <IconButton
-                            onClick={() => {
-                              console.log(recipe.id);
-                              //   setShowModal(true);
-                              //   setDeleteCat(cat.uid);
-                            }}>
-                            <TrashIcon />
-                          </IconButton>
-                        </Tooltip>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+          <Grid item>
+            <CircularProgress color='secondary' />
           </Grid>
         </Grid>
       ) : (
-        <Typography variant='subtitle1'>no favorite recipes found</Typography>
+        <Grid container item direction='row' justify='center'>
+          {state.favorites.length > 0 ? (
+            <Grid item xs={12}>
+              <Fade in={!state.loading}>
+                <TableContainer component={Paper}>
+                  <Table aria-label='simple table'>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Image</TableCell>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Category</TableCell>
+                        <TableCell>Public/Private</TableCell>
+                        <TableCell>Actions</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {state.favorites.map(recipe => (
+                        <TableRow key={recipe.id}>
+                          <TableCell component='th' scope='row'>
+                            <Tooltip title='View Recipe'>
+                              <Link to={`/recipe/${recipe.id}`}>
+                                <img src={recipe.img} alt='placeholder' />
+                              </Link>
+                            </Tooltip>
+                          </TableCell>
+                          <TableCell>{recipe.title}</TableCell>
+                          <TableCell>{recipe.category}</TableCell>
+                          <TableCell>
+                            {recipe.public ? "public" : "private"}
+                          </TableCell>
+                          <TableCell>
+                            <Tooltip title='Remove from Favorites'>
+                              <IconButton
+                                onClick={() => removeFavorite(recipe.id)}>
+                                <TrashIcon />
+                              </IconButton>
+                            </Tooltip>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Fade>
+            </Grid>
+          ) : (
+            <React.Fragment>
+              <Grid item>
+                <Typography variant='subtitle1'>
+                  no favorite recipes found
+                </Typography>
+                <button onClick={() => setFavorites()}>
+                  POPULATE DUMMY DATA
+                </button>
+              </Grid>
+            </React.Fragment>
+          )}
+        </Grid>
       )}
     </React.Fragment>
   );
