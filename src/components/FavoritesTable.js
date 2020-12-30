e8d70d4a25c3f739900add4f3e62cef7dfdf2a6a
dansirdan <daniel.monteton@gmail.com>
2020-12-30 14:14:00 -0800

    created favorites page table, dummy data, on image click and delete from favorites click

--- src/components/FavoritesTable.js
+++ src/components/FavoritesTable.js
@@ -1,4 +1,4 @@
-import React, {} from "react";
+import React from "react";
 import Table from "@material-ui/core/Table";
 import Grid from "@material-ui/core/Grid";
 import TableBody from "@material-ui/core/TableBody";
@@ -10,15 +10,35 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import TrashIcon from "@material-ui/icons/Delete";
+import { Link } from "react-router-dom";
 // import EditIcon from "@material-ui/icons/Edit";
 // import Avatar from "@material-ui/core/Avatar";
 import Tooltip from "@material-ui/core/Tooltip";
 
-const FavoritesTable = ({
-  favoriteRecipes,
-}) => {
-
-  
+const FavoritesTable = () => {
+  const favoriteRecipes = [
+    {
+      id: 0,
+      img: "https://via.placeholder.com/100",
+      title: "test",
+      category: "soup",
+      public: true,
+    },
+    {
+      id: 2,
+      img: "https://via.placeholder.com/100",
+      title: "test",
+      category: "pasta",
+      public: false,
+    },
+    {
+      id: 3,
+      img: "https://via.placeholder.com/100",
+      title: "test",
+      category: "dessert",
+      public: true,
+    },
+  ];
 
   return (
     <React.Fragment>
@@ -29,6 +49,7 @@ const FavoritesTable = ({
               <Table aria-label='simple table'>
                 <TableHead>
                   <TableRow>
+                    <TableCell>Image</TableCell>
                     <TableCell>Title</TableCell>
                     <TableCell>Category</TableCell>
                     <TableCell>Public/Private</TableCell>
@@ -37,19 +58,26 @@ const FavoritesTable = ({
                 </TableHead>
                 <TableBody>
                   {favoriteRecipes.map(recipe => (
-                    <TableRow key={recipe.uid}>
+                    <TableRow key={recipe.id}>
                       <TableCell component='th' scope='row'>
-                        {recipe.title}
+                        <Tooltip title='View Recipe'>
+                          <Link to={`/recipe/${recipe.id}`}>
+                            <img src={recipe.img} alt='placeholder' />
+                          </Link>
+                        </Tooltip>
                       </TableCell>
+                      <TableCell>{recipe.title}</TableCell>
                       <TableCell>{recipe.category}</TableCell>
-                      <TableCell>{recipe.public ? "public": "private"}</TableCell>
                       <TableCell>
-                        <Tooltip title='Remove Favorite'>
+                        {recipe.public ? "public" : "private"}
+                      </TableCell>
+                      <TableCell>
+                        <Tooltip title='Remove from Favorites'>
                           <IconButton
                             onClick={() => {
-                                console.log(recipe.id);
-                            //   setShowModal(true);
-                            //   setDeleteCat(cat.uid);
+                              console.log(recipe.id);
+                              //   setShowModal(true);
+                              //   setDeleteCat(cat.uid);
                             }}>
                             <TrashIcon />
                           </IconButton>
